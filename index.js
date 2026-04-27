39528f1c0834df9a837227befb0bcca68d8036ae
Nikita Belyaev <n.nikitin8888@gmail.com>
2026-04-27 22:30:00 +0300

    add CLI and fix lint errors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-    return `Comparing ${filepath1} and ${filepath2} in ${format} format`
+  return `Comparing ${filepath1} and ${filepath2} in ${format} format`
 }
 
 export default genDiff
