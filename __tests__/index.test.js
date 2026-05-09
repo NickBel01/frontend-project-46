4c0bd0662879bb46d3da5ef5d77964e447f760fc
Nikita Belyaev <n.nikitin8888@gmail.com>
2026-05-09 17:19:29 +0300

    Jest tests for flat JSON comparison

---
+++ __tests__/index.test.js
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'url'
+import path from 'path'
+import fs from 'fs'
+import genDiff from '../src/index.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const getFixturePath = filename => path.join(__dirname, '__fixtures__', filename)
+const readFixture = filename => fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
+
+test('compare flat JSON files', () => {
+  const filepath1 = getFixturePath('file1.json')
+  const filepath2 = getFixturePath('file2.json')
+  const expected = readFixture('expected.txt')
+
+  expect(genDiff(filepath1, filepath2)).toBe(expected)
+})
