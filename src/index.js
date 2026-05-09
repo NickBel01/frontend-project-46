9afb81151f2e6490380d610dcef7b6901b881291
Nikita Belyaev <n.nikitin8888@gmail.com>
2026-05-09 16:44:20 +0300

    add flat JSON Compasion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+import _ from 'lodash'
 import parseFile from './parsers.js'
 
 const genDiff = (filepath1, filepath2) => {
   const data1 = parseFile(filepath1)
   const data2 = parseFile(filepath2)
 
-  return `File 1: ${JSON.stringify(data1)}\nFile 2: ${JSON.stringify(data2)}`
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)))
+
+  const diff = keys.map((key) => {
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`
+    }
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`
+    }
+    return `    ${key}: ${data1[key]}`
+  })
+
+  return `{\n${diff.join('\n')}\n}`
 }
 
 export default genDiff
